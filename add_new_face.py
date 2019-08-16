e4d372c386895579b974b6b529935543e22e5c76
aaronpp65 <aaron.pp@neuroplex.in>
2019-08-16 13:14:59 +0530

    added comments

--- add_new_face.py
+++ add_new_face.py
@@ -2,6 +2,8 @@ from face_recognizer import FaceRecognizer
 import cv2
 
 fce=FaceRecognizer()
+#path to the image of face to be added
 camera = cv2.imread('/home/phi/Neuroplex/face-recognition-vggface2/mals.jpg')
 camera = cv2.cvtColor(camera, cv2.COLOR_BGR2RGB)
+# name of the person to be added
 fce.add_new_face(camera,'mals')
